6646fbc53c6379623fbda88a7cd16c9439a973da
上人浩平 <joninkohei@shouninkouheinoMacBook-Air.local>
2023-10-07 18:23:38 +0900

    commit

--- random_image/js/app.js
+++ random_image/js/app.js
@@ -1,3 +1,7 @@
 $(function () {
-	console.log("OK");
+	$.each($(".container p"), function(index, elem) {
+		setTimeout(function () {
+			$(elem).addClass("animation");
+		}, 200 * index);
+	});
 });
